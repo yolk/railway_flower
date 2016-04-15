8ec10519e8ca39322ae17ab334eb1cebe4ab1423
Sebastian <sebastian@yo.lk>
2016-04-15 15:55:53 +0200

    Fixed setup for sprockets >= v3

--- lib/railway_flower/engine.rb
+++ lib/railway_flower/engine.rb
@@ -6,7 +6,12 @@ module RailwayFlower
     isolate_namespace RailwayFlower
 
     initializer :setup_railway_flower do |app|
-      app.assets.append_path "#{app.root}/#{RailwayFlower.spec_path}"
+      if (app.assets && app.assets.respond_to?(:append_path))
+        app.assets.append_path "#{app.root}/#{RailwayFlower.spec_path}"
+
+      elsif Sprockets::VERSION.split(".").first.to_i >= 3
+        Sprockets.append_path "#{app.root}/#{RailwayFlower.spec_path}"
+      end
     end
   end
 end
